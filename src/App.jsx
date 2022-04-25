85cf70a69d9f2c039efcd93e647e97c30bcb8deb
JonaTorsson <jona.torsson@gmail.com>
2022-04-25 14:02:57 +0200

    Created navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./components/Navigation";
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+const App = () => {
+  return (
+	  
+      <Navigation />
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+
+   
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+	return (
+		<Navbar bg="dark" variant="dark" expand="md">
+			<Container>
+				<Navbar.Brand as={Link} to="/">Starwars Encyclopedia</Navbar.Brand>
+
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						<Nav.Link as={NavLink} end to="/characters">Characters</Nav.Link>
+						<Nav.Link as={NavLink} to="/films">Films</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	)
+}
+
+export default Navigation
