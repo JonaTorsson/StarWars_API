5e4ce9becb30312f88d77285021e3834ad95e62f
JonaTorsson <jona.torsson@gmail.com>
2022-04-25 19:32:53 +0200

    added serivce map with starwars API

---
+++ src/services/SwapiApi.jsx
@@ -0,0 +1,40 @@
+/**
+ * Starwars API
+ * 
+ */
+
+import axios from 'axios'
+
+const BASE_URL = 'https://swapi.dev/api/'
+
+// Get ALL films
+const getFilms = async () => {
+	const res = await axios.get(`${BASE_URL}/films`)
+	return res.data
+}
+
+// Get SPECIFIC film
+const getFilm = async (id) => {
+	const res = await axios.get(`${BASE_URL}/films/${id}`)
+	return res.data
+}
+
+// Get ALL characters
+const getCharacters = async () => {
+	const res = await axios.get(`${BASE_URL}/people`)
+	return res.data
+}
+
+// Get SPECIFIC character
+const getCharacter = async (id) => {
+	const res = await axios.get(`${BASE_URL}/people/${id}`)
+	return res.data
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	getFilms,
+	getFilm,
+	getCharacters,
+	getCharacter
+}
